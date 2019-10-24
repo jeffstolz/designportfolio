97309a66f383f3551df4096afa1f4ae78cedde71
Jeff Stolz <jstolz123@gmail.com>
2019-10-24 14:10:45 -0400

    add hero banner

--- src/components/header.js
+++ src/components/header.js
@@ -3,27 +3,54 @@ import { Link } from "gatsby"
 import styled from "styled-components"
 
 const Header = () => (
-  <HeaderContainer>
-    <div>
-      <Link href="/">Jeff Stolz</Link>
-    </div>
-    <HeaderLinkGroup>
-      <Link to="/">Work</Link>
-      <Link to="/">About</Link>
-      <Link to="/">Contact</Link>
-    </HeaderLinkGroup>
-  </HeaderContainer>
+  <HeaderOuterContainer>
+    <HeaderContainer>
+      <div>
+        <Link href="/">Jeff Stolz</Link>
+      </div>
+      <HeaderLinkGroup>
+        <ActiveLinkContainer>
+          <Link to="/">Work</Link>
+        </ActiveLinkContainer>
+        <LinkContainer>
+          <Link to="/">About</Link>
+        </LinkContainer>
+        <LinkContainer>
+          <Link to="/">Contact</Link>
+        </LinkContainer>
+      </HeaderLinkGroup>
+    </HeaderContainer>
+  </HeaderOuterContainer>
 )
+
+const HeaderOuterContainer = styled.header`
+  display: flex;
+  justify-content: center;
+  margin-top: 15pt;
+`
 
 const HeaderContainer = styled.header`
   display: flex;
+  width: 90%;
   justify-content: space-between;
 `
 
 const HeaderLinkGroup = styled.div`
   display: flex;
-  justify-content: space-between;
-  width: 150pt;
+  width: 200pt;
+`
+
+const ActiveLinkContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  border-bottom: 1pt solid black;
+`
+
+const LinkContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
 `
 
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,77 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
+import Header from "../components/header"
 import Image from "../components/image"
 import SEO from "../components/seo"
+
+import styled from "styled-components"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <BannerContainer>
+      <Left>
+        <Divider />
+        <FancyText>
+          <Greeting>Hi, I’m Jeff.</Greeting> I work with teams from the ground
+          up to ideate, design, and implement software. My favorite tools are
+          React and Figma. I’m a big fan of design sprints, design systems, and
+          end user testing.
+        </FancyText>
+        <FancyText>
+          I’m always interested in geeking out about travel, science fiction,
+          and D&D.
+        </FancyText>
+        <Divider />
+      </Left>
+      <Right>
+        <LargeText>
+          PRODUCT <br />
+          DESIGNER, <br />
+          FRONTEND <br />
+          DEVELOPER
+        </LargeText>
+      </Right>
+    </BannerContainer>
   </Layout>
 )
+
+const BannerContainer = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-image: linear-gradient(to right, #6e55c1, #80cbe6);
+  height: 400pt;
+`
+const Left = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 200pt;
+  height: 280pt;
+  margin-right: 20pt;
+`
+
+const FancyText = styled.div`
+  font-family: "georgia", serif;
+`
+
+const Greeting = styled.span`
+  font-weight: 800;
+  font-style: italic;
+`
+
+const Right = styled.div``
+
+const LargeText = styled.h1`
+  font-size: 80pt;
+  line-height: 75pt;
+  margin-bottom: 0;
+`
+
+const Divider = styled.div`
+  border-bottom: 1pt solid black;
+`
 
 export default IndexPage
