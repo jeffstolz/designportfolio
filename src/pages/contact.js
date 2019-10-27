dda3e7643fa829ad75cf91860cd2459c5028e52c
Jeff Stolz <jstolz123@gmail.com>
2019-10-27 15:18:21 -0400

    add resume link

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,8 @@
 import React from "react"
+import { Link } from "gatsby"
 import styled from "styled-components"
+
+import Resume from "../images/jeff_stolz_resume.pdf"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -14,6 +17,11 @@ const ContactPage = () => (
           looking for new opportunities to create, share, and grow as a
           designer. I look forward to hearing from you.
         </FancyText>
+        <DownloadContainer>
+          <DownloadButton href={Resume} download>
+            Download Resume
+          </DownloadButton>
+        </DownloadContainer>
       </TextContainer>
     </Container>
     <SEO title="Contact" />
@@ -25,10 +33,12 @@ const Container = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  height: 520pt;
-  margin-bottom: 40pt;
+  height: 550pt;
+  margin-top: 25pt;
 
   @media (max-width: 950px) {
+    height: 450pt;
+    margin-top: 128pt;
     padding-left: 20pt;
     padding-right: 20pt;
   }
@@ -56,6 +66,39 @@ const TextContainer = styled.div`
 
 const FancyText = styled.div`
   font-family: "georgia", serif;
+`
+
+const DownloadContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  margin-top: 30pt;
+`
+
+const DownloadButton = styled.a`
+  display: flex;
+  width: 200pt;
+  height: 50pt;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid black;
+  border-radius: 25pt;
+  -webkit-box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16);
+  -moz-box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16);
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16);
+
+  &:hover {
+    background-color: black;
+    text-decoration: none;
+    color: white;
+    -webkit-box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25);
+    -moz-box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25);
+    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25);
+  }
+
+  @media (max-width: 950px) {
+    margin-top: 50pt;
+  }
 `
 
 export default ContactPage
