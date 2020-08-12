a383a571078123c9e6700bc99696dd5256183160
Jeff Stolz <jstolz123@gmail.com>
2020-08-12 15:41:36 -0400

    Refactor global styling

--- src/layouts/home-layout.js
+++ src/layouts/home-layout.js
@@ -1,11 +1,14 @@
 import React from "react"
+import StylingResets from "../styles/styling-resets"
+import GlobalStyles from "../styles/global-styles"
 import Navbar from "../components/navbar"
 import Footer from "../components/footer"
-import "../styles/global-styles.css"
 
 const HomeLayout = ({ children }) => {
   return (
     <>
+      <StylingResets />
+      <GlobalStyles />
       <Navbar />
       <main>{children}</main>
       <Footer />
