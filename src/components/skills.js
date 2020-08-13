eaaf1a4360e07b143dd1efb493839c1a0c339499
Jeff Stolz <jstolz123@gmail.com>
2020-08-13 18:12:38 -0400

    Add background images and colors (#58)

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,106 +1,116 @@
 import React from "react"
 import styled from "styled-components"
 import SkillsCard from "./skills-card"
+import { Images } from "../images"
 import { Colors, Spacing } from "../styles/variables"
 
 const Skills = () => (
   <OuterContainer id="skills">
-    <Container>
-      <DesignContainer>
-        <div>
-          <Header>How I Design</Header>
-          <CardList>
-            <SkillsCard
-              heading={"Jobs-To-Be-Done"}
-              text={
-                "Ensuring we're solving the right problem is my first step to every project."
-              }
-            />
-            <SkillsCard
-              heading={"Personas"}
-              text={
-                "They can be rough, but ensuring we always maintain user context is crucial to success."
-              }
-            />
-            <SkillsCard
-              heading={"GV Design Sprints"}
-              text={
-                "An excellent guideline, although I've written about how sprints never quite go as planned."
-              }
-            />
-            <SkillsCard
-              heading={"Atomic Design Systems"}
-              text={
-                "Always a fan of this methodology for larger projects, especially when paired with React."
-              }
-            />
-            <SkillsCard
-              heading={"Figma"}
-              text={
-                "I'll work with anything, but I prefer Figma for being cloud-based & platform agnostic."
-              }
-            />
-            <SkillsCard
-              heading={"Informal User Testing"}
-              text={
-                "Getting buy-in can be hard but validation is crucial. I opt for frequency over formality."
-              }
-            />
-          </CardList>
-        </div>
-      </DesignContainer>
-      <DevelopContainer>
-        <div>
-          <Header>How I Develop</Header>
-          <CardList>
-            <SkillsCard
-              heading={"React"}
-              text={
-                "I prefer React for it's component hierarchy. This repo is a good example of how I code."
-              }
-            />
-            <SkillsCard
-              heading={"React Native"}
-              text={
-                "For cross-platform apps, I find a single repo & team makes for smoother deliverables."
-              }
-            />
-            <SkillsCard
-              heading={"Accessibility Always"}
-              text={
-                "I see accessibile design & development as a responsibility. I'm a big fan of the A11y Project."
-              }
-            />
-            <SkillsCard
-              heading={"Ship Early & Often"}
-              text={
-                "Especially for MVPs, overbearing code review policies & CI can often hurt more than help."
-              }
-            />
-            <SkillsCard
-              heading={"Concise Pull Requests"}
-              text={
-                "I prefer to feature branch and keep my PRs focused & squashed into single commits."
-              }
-            />
-            <SkillsCard
-              heading={"Prioritize Mobile"}
-              text={
-                "Many users only have a mobile device to access the internet - let's keep them in mind."
-              }
-            />
-          </CardList>
-        </div>
-      </DevelopContainer>
-    </Container>
+    <BackgroundOverlay>
+      <Container>
+        <DesignContainer>
+          <div>
+            <Header>How I Design</Header>
+            <CardList>
+              <SkillsCard
+                heading={"Jobs-To-Be-Done"}
+                text={
+                  "Ensuring we're solving the right problem is my first step to every project."
+                }
+              />
+              <SkillsCard
+                heading={"Personas"}
+                text={
+                  "They can be rough, but ensuring we always maintain user context is crucial to success."
+                }
+              />
+              <SkillsCard
+                heading={"GV Design Sprints"}
+                text={
+                  "An excellent guideline, although I've written about how sprints never quite go as planned."
+                }
+              />
+              <SkillsCard
+                heading={"Atomic Design Systems"}
+                text={
+                  "Always a fan of this methodology for larger projects, especially when paired with React."
+                }
+              />
+              <SkillsCard
+                heading={"Figma"}
+                text={
+                  "I'll work with anything, but I prefer Figma for being cloud-based & platform agnostic."
+                }
+              />
+              <SkillsCard
+                heading={"Informal User Testing"}
+                text={
+                  "Getting buy-in can be hard but validation is crucial. I opt for frequency over formality."
+                }
+              />
+            </CardList>
+          </div>
+        </DesignContainer>
+        <DevelopContainer>
+          <div>
+            <Header>How I Develop</Header>
+            <CardList>
+              <SkillsCard
+                heading={"React"}
+                text={
+                  "I prefer React for it's component hierarchy. This repo is a good example of how I code."
+                }
+              />
+              <SkillsCard
+                heading={"React Native"}
+                text={
+                  "For cross-platform apps, I find a single repo & team makes for smoother deliverables."
+                }
+              />
+              <SkillsCard
+                heading={"Accessibility Always"}
+                text={
+                  "I see accessibile design & development as a responsibility. I'm a big fan of the A11y Project."
+                }
+              />
+              <SkillsCard
+                heading={"Ship Early & Often"}
+                text={
+                  "Especially for MVPs, overbearing code review policies & CI can often hurt more than help."
+                }
+              />
+              <SkillsCard
+                heading={"Concise Pull Requests"}
+                text={
+                  "I prefer to feature branch and keep my PRs focused & squashed into single commits."
+                }
+              />
+              <SkillsCard
+                heading={"Prioritize Mobile"}
+                text={
+                  "Many users only have a mobile device to access the internet - let's keep them in mind."
+                }
+              />
+            </CardList>
+          </div>
+        </DevelopContainer>
+      </Container>
+    </BackgroundOverlay>
   </OuterContainer>
 )
 
 const OuterContainer = styled.section`
+  background-image: linear-gradient(-25deg, ${Colors.blue}, ${Colors.cyan});
+`
+
+const BackgroundOverlay = styled.div`
   display: flex;
   justify-content: center;
-  background-color: ${Colors.lightGray};
   padding: ${Spacing.huge} ${Spacing.base};
+  background-image: url(${Images.DesignSkills}), url(${Images.DevelopSkills});
+  background-position: top right, bottom left;
+  background-repeat: no-repeat;
+  background-size: 50%;
 `
 
 const Container = styled.div`
--- src/images/index.js
+++ src/images/index.js
@@ -1,62 +1,69 @@
-import CipherbioArchives from './cipherbio-archives.png';
-import CipherbioArticle from './cipherbio-article.png';
-import CipherbioBlogArticle from './cipherbio-blog-article.png';
-import CipherbioBlog from './cipherbio-blog.png';
-import CipherbioCommunity from './cipherbio-blog.png';
-import CipherbioCompany from './cipherbio-company.png';
-import CipherbioThumbnail from './cipherbio-thumbnail.png';
-import Favicon from './favicon.png';
-import GeAirspace from './ge-airspace.png';
-import GeAuth from './ge-auth.png';
-import GeCollapsed from './ge-collapsed.png';
-import GeCompliance from './ge-compliance.png';
-import GeDraw from './ge-draw.png';
-import GeLayers from './ge-layers.png';
-import GeRequests from './ge-requests.png';
-import GeSearch from './ge-search.png';
-import GeThumbnail from './ge-thumbnail.png';
-import GeTooltip from './ge-tooltip.png';
-import GeWeather from './ge-weather.png';
-import HmhMap from './hmh-map.jpg';
-import HmhOverview from './hmh-overview.jpg';
-import HmhSketches from './hmh-sketches.jpg';
-import HmhSolutionOne from './hmh-solution-1.jpg';
-import HmhSolutionTwo from './hmh-solution-2.jpg';
-import HmhSolutionThree from './hmh-solution-3.jpg';
-import HmhSolutionFour from './hmh-solution-4.jpg';
-import HmhSolutionFive from './hmh-solution-5.jpg';
-import HmhThumbnail from './hmh-thumbnail.png';
-import HmhUxcanvas from './hmh-uxcanvas.jpg';
-import JeffStolzResume from './jeff_stolz_resume.pdf';
-import JeffStolz from './jeff-stolz.png';
-import MhDesignProcessSketch from './mh-design-process-sketch.jpg';
-import MhDiscovery from './mh-discovery.jpg';
-import MhOverview from './mh-overview.jpg';
-import MhPersona from './mh-persona.jpg';
-import MhSolutionOne from './mh-solution-1.jpg';
-import MhSolutionTwo from './mh-solution-2.jpg';
-import MhSolutionThree from './mh-solution-3.jpg';
-import MhSolutionFour from './mh-solution-4.jpg';
-import MhSolutionFive from './mh-solution-5.jpg';
-import MhSolutionSix from './mh-solution-6.jpg';
-import MhThumbnail from './mh-thumbnail.png';
-import MhWireframe from './mh-wireframe.jpg';
-import SkillsAthleteLanding from './skills-athlete-landing.jpg';
-import SkillsAthlete from './skills-athlete.jpg';
-import SkillsAthletes from './skills-athletes.jpg';
-import SkillsFlowDiagram from './skills-flow-diagram.png';
-import SkillsHome from './skills-home.jpg';
-import SkillsMoodboardOne from './skills-moodboard-1.jpg';
-import SkillsMoodboardTwo from './skills-moodboard-2.jpg';
-import SkillsNotes from './skills-notes.png';
-import SkillsThumbnail from './skills-thumbnail.png';
-import SkillsVideo from './skills-video.jpg';
-import SkillsWireframes from './skills-wireframes.jpg';
-import ViralgainsThumbnail from './vg-thumbnail.png';
-import ViralgainsOne from './viralgains-1.png';
-import ViralgainsTwo from './viralgains-2.png';
-import ViralgainsThree from './viralgains-3.png';
-import ViralgainsFour from './viralgains-4.png';
+import CipherbioArchives from "./cipherbio-archives.png"
+import CipherbioArticle from "./cipherbio-article.png"
+import CipherbioBlogArticle from "./cipherbio-blog-article.png"
+import CipherbioBlog from "./cipherbio-blog.png"
+import CipherbioCommunity from "./cipherbio-blog.png"
+import CipherbioCompany from "./cipherbio-company.png"
+import CipherbioThumbnail from "./cipherbio-thumbnail.png"
+import CoffeeContact from "./coffee-contact.svg"
+import DesignIntro from "./design-intro.svg"
+import DesignSkills from "./design-skills.svg"
+import DevelopIntro from "./develop-intro.svg"
+import DevelopSkills from "./develop-skills.svg"
+import Favicon from "./favicon.png"
+import GeAirspace from "./ge-airspace.png"
+import GeAuth from "./ge-auth.png"
+import GeCollapsed from "./ge-collapsed.png"
+import GeCompliance from "./ge-compliance.png"
+import GeDraw from "./ge-draw.png"
+import GeLayers from "./ge-layers.png"
+import GeRequests from "./ge-requests.png"
+import GeSearch from "./ge-search.png"
+import GeThumbnail from "./ge-thumbnail.png"
+import GeTooltip from "./ge-tooltip.png"
+import GeWeather from "./ge-weather.png"
+import HospitalEthos from "./hospital-ethos.svg"
+import HmhMap from "./hmh-map.jpg"
+import HmhOverview from "./hmh-overview.jpg"
+import HmhSketches from "./hmh-sketches.jpg"
+import HmhSolutionOne from "./hmh-solution-1.jpg"
+import HmhSolutionTwo from "./hmh-solution-2.jpg"
+import HmhSolutionThree from "./hmh-solution-3.jpg"
+import HmhSolutionFour from "./hmh-solution-4.jpg"
+import HmhSolutionFive from "./hmh-solution-5.jpg"
+import HmhThumbnail from "./hmh-thumbnail.png"
+import HmhUxcanvas from "./hmh-uxcanvas.jpg"
+import JeffStolzResume from "./jeff_stolz_resume.pdf"
+import JeffStolz from "./jeff-stolz.png"
+import MhDesignProcessSketch from "./mh-design-process-sketch.jpg"
+import MhDiscovery from "./mh-discovery.jpg"
+import MhOverview from "./mh-overview.jpg"
+import MhPersona from "./mh-persona.jpg"
+import MhSolutionOne from "./mh-solution-1.jpg"
+import MhSolutionTwo from "./mh-solution-2.jpg"
+import MhSolutionThree from "./mh-solution-3.jpg"
+import MhSolutionFour from "./mh-solution-4.jpg"
+import MhSolutionFive from "./mh-solution-5.jpg"
+import MhSolutionSix from "./mh-solution-6.jpg"
+import MhThumbnail from "./mh-thumbnail.png"
+import MhWireframe from "./mh-wireframe.jpg"
+import SchoolEthos from "./school-ethos.svg"
+import SkillsAthleteLanding from "./skills-athlete-landing.jpg"
+import SkillsAthlete from "./skills-athlete.jpg"
+import SkillsAthletes from "./skills-athletes.jpg"
+import SkillsFlowDiagram from "./skills-flow-diagram.png"
+import SkillsHome from "./skills-home.jpg"
+import SkillsMoodboardOne from "./skills-moodboard-1.jpg"
+import SkillsMoodboardTwo from "./skills-moodboard-2.jpg"
+import SkillsNotes from "./skills-notes.png"
+import SkillsThumbnail from "./skills-thumbnail.png"
+import SkillsVideo from "./skills-video.jpg"
+import SkillsWireframes from "./skills-wireframes.jpg"
+import ViralgainsThumbnail from "./vg-thumbnail.png"
+import ViralgainsOne from "./viralgains-1.png"
+import ViralgainsTwo from "./viralgains-2.png"
+import ViralgainsThree from "./viralgains-3.png"
+import ViralgainsFour from "./viralgains-4.png"
 
 export const Images = {
   CipherbioArchives,
@@ -66,6 +73,11 @@ export const Images = {
   CipherbioCommunity,
   CipherbioCompany,
   CipherbioThumbnail,
+  CoffeeContact,
+  DesignIntro,
+  DesignSkills,
+  DevelopIntro,
+  DevelopSkills,
   Favicon,
   GeAirspace,
   GeAuth,
@@ -88,6 +100,7 @@ export const Images = {
   HmhSolutionFive,
   HmhThumbnail,
   HmhUxcanvas,
+  HospitalEthos,
   JeffStolzResume,
   JeffStolz,
   MhDesignProcessSketch,
@@ -102,6 +115,7 @@ export const Images = {
   MhSolutionSix,
   MhThumbnail,
   MhWireframe,
+  SchoolEthos,
   SkillsAthleteLanding,
   SkillsAthlete,
   SkillsAthletes,
@@ -118,4 +132,4 @@ export const Images = {
   ViralgainsTwo,
   ViralgainsThree,
   ViralgainsFour,
-};
+}
