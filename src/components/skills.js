27f94ce8f209a315c96d696b0e16e6f820725c12
Jeff Stolz <jstolz123@gmail.com>
2020-08-14 13:43:49 -0400

    Add skills section icons

--- src/components/skills.js
+++ src/components/skills.js
@@ -13,39 +13,53 @@ const Skills = () => (
             <Header>How I Design</Header>
             <CardList>
               <SkillsCard
+                imageSrc={Images.Jtbd}
+                imageAlt={"Icon of a whiteboard with sections of writing"}
                 heading={"Jobs-To-Be-Done"}
                 text={
-                  "Ensuring we're solving the right problem is my first step to every project."
+                  "Ensuring we're solving the right problems, for the right people, is my first step to every project."
                 }
               />
               <SkillsCard
+                imageSrc={Images.Persona}
+                imageAlt={"Icon of a user persona"}
                 heading={"Personas"}
                 text={
-                  "They can be rough, but ensuring we always maintain user context is crucial to success."
+                  "I tend to blend these with JTBD to ensure we maintain user empathy throughout the project."
                 }
               />
               <SkillsCard
+                imageSrc={Images.Pds}
+                imageAlt={"Icon of a user journey diagram"}
                 heading={"GV Design Sprints"}
                 text={
                   "An excellent guideline, although I've written about how sprints never quite go as planned."
                 }
               />
               <SkillsCard
+                imageSrc={Images.Atomic}
+                imageAlt={"Icon of three atoms linked together"}
                 heading={"Atomic Design Systems"}
                 text={
                   "Always a fan of this methodology for larger projects, especially when paired with React."
                 }
               />
               <SkillsCard
+                imageSrc={Images.Figma}
+                imageAlt={"Figma logo"}
                 heading={"Figma"}
                 text={
-                  "I'll work with anything, but I prefer Figma for being cloud-based & platform agnostic."
+                  "I'll work with anything, but I prefer Figma for it's cloud-based & platform agnostic architecture."
                 }
               />
               <SkillsCard
+                imageSrc={Images.Interview}
+                imageAlt={
+                  "Icon of two users with a shared chat bubble above them"
+                }
                 heading={"Informal User Testing"}
                 text={
-                  "Getting buy-in can be hard but validation is crucial. I opt for frequency over formality."
+                  "Stakeholder buy-in can be hard but validation is crucial. I opt for frequency over formality."
                 }
               />
             </CardList>
@@ -56,36 +70,48 @@ const Skills = () => (
             <Header>How I Develop</Header>
             <CardList>
               <SkillsCard
+                imageSrc={Images.React}
+                imageAlt={"React Logo"}
                 heading={"React"}
                 text={
                   "I prefer React for it's component hierarchy. This repo is a good example of how I code."
                 }
               />
               <SkillsCard
+                imageSrc={Images.React}
+                imageAlt={"React Native Logo"}
                 heading={"React Native"}
                 text={
                   "For cross-platform apps, I find a single repo & team makes for smoother deliverables."
                 }
               />
               <SkillsCard
+                imageSrc={Images.A11y}
+                imageAlt={"The A11y Project Logo"}
                 heading={"Accessibility Always"}
                 text={
                   "I see accessibile design & development as a responsibility. I'm a big fan of the A11y Project."
                 }
               />
               <SkillsCard
+                imageSrc={Images.Ship}
+                imageAlt={"Icon of a rocket ship"}
                 heading={"Ship Early & Often"}
                 text={
                   "Especially for MVPs, overbearing code review policies & CI can often hurt more than help."
                 }
               />
               <SkillsCard
+                imageSrc={Images.Github}
+                imageAlt={"Github Logo"}
                 heading={"Concise Pull Requests"}
                 text={
                   "I prefer to feature branch and keep my PRs focused & squashed into single commits."
                 }
               />
               <SkillsCard
+                imageSrc={Images.Mobile}
+                imageAlt={"Icon of a mobile phone"}
                 heading={"Prioritize Mobile"}
                 text={
                   "Many users only have a mobile device to access the internet - let's keep them in mind."
