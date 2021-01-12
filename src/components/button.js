10daa47ca8346564fd404804f68f9df4c00f9905
Jeff Stolz <jstolz123@gmail.com>
2021-01-11 21:25:17 -0500

    Refactor button component and add themes

---
+++ src/components/button.js
@@ -0,0 +1,47 @@
+import React from "react"
+import styled from "styled-components"
+import { Images } from "../images"
+import { Colors, Typography } from "../styles/variables"
+
+const Button = ({ label, icon }) => (
+  <Container href={Images.JeffStolzResume} download>
+    {label}
+    <IconContainer>{icon}</IconContainer>
+  </Container>
+)
+
+const Container = styled.a`
+  position: relative;
+  font-family: ${Typography.headingFontFamily};
+  font-size: ${Typography.xSmallFontSize};
+  font-weight: ${Typography.mediumFontWeight};
+  background-color: ${props => props.theme.bgButton};
+  color: ${props => props.theme.primaryButton};
+  padding: 1.3em 2.8em 1.3em 1.6em;
+  text-decoration: none;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  -webkit-transition: all 0.25s linear;
+  transition: all 0.25s linear;
+
+  &:hover {
+    background-color: ${props => props.theme.primaryButton};
+    color: ${props => props.theme.secondaryButton};
+  }
+`
+
+Container.defaultProps = {
+  theme: {
+    bgButton: Colors.lightGray,
+    primaryButton: Colors.black,
+    secondaryButton: Colors.white,
+  },
+}
+
+const IconContainer = styled.span`
+  position: absolute;
+  top: 1.4em;
+  right: 1.3em;
+`
+
+export default Button
---
+++ src/components/themes.js
@@ -0,0 +1,7 @@
+import { Colors } from "../styles/variables"
+
+export const darkTheme = {
+  bgButton: Colors.darkGray,
+  primaryButton: Colors.white,
+  secondaryButton: Colors.black,
+}
