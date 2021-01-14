22eaf3db77326218cf3c43b3c308d1e13de28c9f
Jeff Stolz <jstolz123@gmail.com>
2021-01-14 11:15:23 -0500

    Redesign contact section

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,107 +1,94 @@
 import React from "react"
-import styled from "styled-components"
-import { Images } from "../images"
-import { Colors, Spacing, Typography } from "../styles/variables"
+import styled, { ThemeProvider } from "styled-components"
+import Fade from "react-reveal/Fade"
+import Button from "./button"
+import { FaArrowDown } from "react-icons/fa"
+import { darkTheme } from "./themes"
+import { Colors, Spacing } from "../styles/variables"
 
 const Contact = () => (
-  <OuterContainer>
-    <BackgroundOverlay>
-      <Container>
-        <Heading>Let's Chat!</Heading>
-        <Text>
-          Thanks for taking the time to review my site. I am always looking for
-          new opportunities to create, share, and grow as a designer. Please
-          reach out if you'd like to chat over a (remote?) coffee. I look
-          forward to hearing from you!
-        </Text>
-        <LinkList>
-          <LinkItem>
-            <LinkText href="mailto:j%73%74o%6Cz12%33@%67ma%69%6C.co%6D">
-              Email
-            </LinkText>
-          </LinkItem>
-          <LinkItem>
-            <LinkText href="https://github.com/jeffstolz">Github</LinkText>
-          </LinkItem>
-          <LinkItem>
-            <LinkText href="https://www.linkedin.com/in/jeff-stolz-17666725/">
-              LinkedIn
-            </LinkText>
-          </LinkItem>
-          <LinkItem>
-            <LinkText href={Images.JeffStolzResume} download>
-              Resume
-            </LinkText>
-          </LinkItem>
-        </LinkList>
-      </Container>
-    </BackgroundOverlay>
-  </OuterContainer>
+  <Container>
+    <Fade>
+      <Heading>Let's Chat!</Heading>
+    </Fade>
+    <Fade>
+      <Text>
+        Thanks for taking the time to review my site. I am always looking for
+        new opportunities to create, share, and grow as a designer. Please reach
+        out if you'd like to chat over a (remote?) coffee. I look forward to
+        hearing from you!
+      </Text>
+    </Fade>
+    <Fade>
+      <LinkList>
+        <LinkItem>
+          <LinkText href="mailto:j%73%74o%6Cz12%33@%67ma%69%6C.co%6D">
+            Email
+          </LinkText>
+          <Slash aria-hidden="true">/</Slash>
+        </LinkItem>
+        <LinkItem>
+          <LinkText href="https://github.com/jeffstolz">Github</LinkText>
+          <Slash aria-hidden="true">/</Slash>
+        </LinkItem>
+        <LinkItem>
+          <LinkText href="https://www.linkedin.com/in/jeff-stolz-17666725/">
+            LinkedIn
+          </LinkText>
+        </LinkItem>
+      </LinkList>
+    </Fade>
+    <Fade>
+      <ThemeProvider theme={darkTheme}>
+        <Button label={"Download Resume"} icon={<FaArrowDown />} />
+      </ThemeProvider>
+    </Fade>
+  </Container>
 )
-
-const OuterContainer = styled.section`
-  background-image: linear-gradient(-25deg, ${Colors.blue}, ${Colors.cyan});
-`
-
-const BackgroundOverlay = styled.div`
-  display: flex;
-  justify-content: center;
-  padding: ${Spacing.huge} ${Spacing.base};
-  background-image: url(${Images.CoffeeContact});
-  background-position: center right;
-  background-repeat: no-repeat;
-  background-size: 31em;
-
-  @media (max-width: ${Spacing.smallBreakPoint}) {
-    padding: ${Spacing.large} ${Spacing.small};
-    background-size: 18em;
-  }
-`
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  width: ${Spacing.containerWidth};
+  justify-content: center;
+  min-height: 100vh;
+  padding: 0 12%;
+  background-color: ${Colors.black};
+
+  @media (max-width: ${Spacing.smallBreakPoint}) {
+    padding: 0 8%;
+  }
 `
 
 const Heading = styled.h2`
+  color: ${Colors.white};
   margin-bottom: ${Spacing.small};
 `
 
 const Text = styled.p`
-  font-size: ${Typography.mediumFontSize};
-  line-height: ${Typography.hugeLineHeight};
-  margin-left: ${Spacing.small};
-  padding-bottom: ${Spacing.small};
-
-  @media (max-width: ${Spacing.smallBreakPoint}) {
-    font-size: ${Typography.baseFontSize};
-    margin-left: 0;
-  }
+  color: ${Colors.gray2};
+  max-width: 47em;
 `
 
 const LinkList = styled.ul`
   display: flex;
-  padding-top: ${Spacing.small};
-
-  @media (max-width: ${Spacing.smallBreakPoint}) {
-    flex-direction: column;
-  }
+  margin: ${Spacing.medium} 0;
 `
 
 const LinkItem = styled.li`
-  font-size: ${Typography.mediumFontSize};
-  margin-left: ${Spacing.small};
-
-  @media (max-width: ${Spacing.smallBreakPoint}) {
-    font-size: ${Typography.baseFontSize};
-    margin-left: 0;
-    margin-bottom: ${Spacing.small};
-  }
+  margin-right: ${Spacing.small};
 `
 
 const LinkText = styled.a`
-  color: ${Colors.black};
+  color: ${Colors.white};
+
+  &:hover {
+    color: ${Colors.gray1};
+  }
+`
+
+const Slash = styled.span`
+  margin-left: ${Spacing.small};
+  color: ${Colors.gray3};
 `
 
 export default Contact
